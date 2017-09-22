f23944bdff1a3dd6d40aab2b5f1e26bca5d42e17
unknown <>
2017-09-22 20:52:50 +0800

    优化int、str

--- Interns.py
+++ Interns.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 –*-
 
 PyIntInterns = {}
+PyStrInterns = {}
 PyTupleInterns = {}
 
 def obj_intern(o):
@@ -16,50 +17,26 @@ def obj_intern(o):
 def int_intern(i):
     if type(i) is not int:
         return
+    if -5 <= i <= 256:
+        return
     return PyIntInterns.setdefault(i, i)
 
 def str_intern(s):
     if type(s) is not str:
         return
-    return intern(s)
+    if len(s) <= 1:
+        return
+    #return intern(s)
+    return PyStrInterns.setdefault(s, s)
 
 def tuple_intern(t):
     if type(t) is not tuple:
+        return
+    if len(t) == 0:
         return
     t2 = tuple([obj_intern(o) for o in t])
     assert t == t2
     return PyTupleInterns.setdefault(t2, t2)
 
 if __name__ == "__main__":
-    #test int obj intern
-    with open("int.txt", "r") as f:
-        print "int obj before intern:"
-        intobjs = [int(l) for l in f.readlines()]
-        for i in intobjs:
-            print i, id(i)
-        print "int obj after intern:"
-        intobjs = [obj_intern(i) for i in intobjs]
-        for i in intobjs:
-            print i, id(i)
-    
-    #test str obj intern
-    with open("str.txt", "r") as f:
-        print "str obj before intern:"
-        strobjs = [str(l).strip() for l in f.readlines()]
-        for s in strobjs:
-            print s, id(s)
-        print "str obj after intern:"
-        strobjs = [obj_intern(s) for s in strobjs]
-        for s in strobjs:
-            print s, id(s)
-    
-    #test tuple obj intern
-    with open("tuple.txt", "r") as f:
-        print "tuple obj before intern:"
-        tupleobjs = [eval(l) for l in f.readlines()]
-        for t in tupleobjs:
-            print t, id(t)
-        print "tuple obj after intern:"
-        tupleobjs = [obj_intern(t) for t in tupleobjs]
-        for t in tupleobjs:
-            print t, id(t)
+    pass
